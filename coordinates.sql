06ca25313881d020a7129a10effb25d90c9e0497
Anna Lednyova <ashtart@gmail.com>
2023-08-26 23:05:51 +0300

    coordinates TABLE Primary Key

---
+++ coordinates.sql
@@ -0,0 +1,13 @@
+CREATE TABLE coordinates(
+    x int,
+    y int,
+    PRIMARY KEY(x, y)
+);
+
+INSERT INTO coordinates VALUES
+    (1, 1),
+    (1, 2),
+    (1, 4)
+    ;
+
+INSERT INTO coordinates VALUES(1, 2);
