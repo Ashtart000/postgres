34aab5ca0728330fc1ed3357d9f7ad7910fb97de
Anna Lednyova <ashtart@gmail.com>
2023-09-09 22:37:03 +0300

    functions

--- any--in-some-exists.sql
+++ any--in-some-exists.sql
@@ -82,3 +82,28 @@ ON otp.product_id = p.id);
 
 SELECT * FROM all_users_with_all_orders
 WHERE user_id = 10;
+
+
+-- розбір дз
+
+--- Знайти всі телефони, які купував юзер з id 11
+-- Не використовуючи JOINів, за допомогою виразів підзапитів
+
+-- варіант без JOIN'ів
+SELECT * FROM products AS p
+WHERE p.id = ANY (
+    SELECT product_id FROM orders_to_products AS otp 
+    WHERE otp.order_id = SOME (
+        SELECT id FROM orders AS o
+        WHERE customer_id = 10
+    )
+);
+
+
+--варіант з JOIN'ами
+SELECT * FROM products AS p
+INNER JOIN orders_to_products AS otp
+ON otp.product_id = p.id
+INNER JOIN orders AS o
+ON otp.order_id = o.id
+WHERE o.customer_id = 11;
